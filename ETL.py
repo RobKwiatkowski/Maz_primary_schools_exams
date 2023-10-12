b6a1fde8b8be159a93bce5765150916a04734c67
Robert <robert.kwiatkowski01@gmail.com>
2023-10-12 19:30:18 +0200

    add requirements.txt add type annotations in ETL.py

--- ETL.py
+++ ETL.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import os.path
 
 
-def add_prefix_to_subset(df: pd.DataFrame, prefix: str):
+def read_raw_data(path:str, sheet_number=1) -> pd.DataFrame:
+    if os.path.isfile(path):
+        data = pd.read_excel(path, sheet_name=sheet_number)
+    else:
+        raise FileExistsError
+
+    return data
+
+
+def add_prefix_to_subset(df: pd.DataFrame, prefix: str) -> None:
     col_list = df.columns
     df.columns = col_list.str.replace("\s\((.*)", "", regex=True)
     df.rename({'wynik średni': prefix+'wynik średni',
